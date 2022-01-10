e95540b2b5a18e99179437f1fa73d8bc33dda3ec
Ashwin Kumar Uppala <53075480+ashwinexe@users.noreply.github.com>
2022-01-10 15:51:58 +0530

    fix:✅env added

--- twilio.js
+++ twilio.js
@@ -1,6 +1,8 @@
+require('dotenv').config();
+
 const accountSid = process.env.TWILIO_ACCOUNT_SID;
 const authToken = process.env.TWILIO_ACCOUNT_TOKEN;
-const client = require('twilio')("AC8dff626029f477398d636f25b03f1f00", "1f6ce05ded7f95983c04b1fa7c922f98");
+const client = require('twilio')(accountSid, authToken);
 
 client.messages.create(
     {
